df53c3b8b66149b9054e6d1e157739b570afd806
AidanVicars <49545553+AidanVicars@users.noreply.github.com>
2021-08-09 15:19:25 +0800

    Added comments.

--- main.cpp
+++ main.cpp
@@ -7,11 +7,9 @@ int main()
 	//Init glfw
 	if (!glfwInit())
 	{
-
 		printf("Failed to initialize GLFW\n");
 
 		return -1;
-
 	}
 		
 	//Hint to GLFW what version we're using
@@ -24,30 +22,24 @@ int main()
 
 	if (window == nullptr)
 	{
-
 		printf_s("Failed to create window! Terminating...\n");
 
 		glfwTerminate();
 		return -1;
-
 	}
+	
 	//Associate context with the new window
 	glfwMakeContextCurrent(window);
-
 	//Dynamically link OpenGL with glad
 	gladLoadGL();
 	//Create the viewport
 	glViewport(0, 0, 1280, 720);
 
-
+	//Load compile and link shaders
 	unsigned int shader_program = shader::create_shader();
 	glUseProgram(shader_program);
-
+	
 	gui::init(window);
-
-	//Update by swapping front and back buffers.
-	glfwSwapBuffers(window);
-
 	//Main window loop
 	while (!glfwWindowShouldClose(window))
 	{
@@ -69,7 +61,6 @@ int main()
 
 	//Destroy ImGui and it's contexts
 	gui::destroy();
-
 	glDeleteProgram(shader_program);
 
 	//Exit and destroy
@@ -77,4 +68,5 @@ int main()
 	glfwTerminate();
 
 	return 0;
+	
 }
